e20a1901af840fb9e38e1c62c465dcf277ba8cad
John Bieling <john.bieling@gmx.de>
2021-08-23 15:52:48 +0200

    register subdialog

--- src/chrome/content/messengerOL.js
+++ src/chrome/content/messengerOL.js
@@ -20,10 +20,38 @@ function onLoad() {
 	window.printingtoolsng = {};
 	window.printingtoolsng.extension = WL.extension;
 
+	window.printingtoolsng.printObserver = {
+		async observe(subDialogWindow) {
+			// A subDialog has been opened.
+			console.log("subDialog opened: " + subDialogWindow.location.href);
+
+			// We only want to deal with the print subDialog.
+			if (!subDialogWindow.location.href.startsWith("chrome://global/content/print.html?")) {
+				return;
+			}
+
+			// Wait until print-settings in the subDialog have been loaded/rendered.
+			await new Promise(resolve =>
+				subDialogWindow.document.addEventListener("print-settings", resolve, { once: true })
+			);
+
+			console.log("subDialog print-settings loaded");
+			console.log("subDialog print-settings caller/opener: " + subDialogWindow.PrintEventHandler.activeCurrentURI);
+
+			/*Services.scriptloader.loadSubScript(
+				"chrome://calendar/content/calendar-print.js",
+				subDialogWindow
+			);*/
+		},
+	};
+
+	Services.obs.addObserver(window.printingtoolsng.printObserver, "subdialog-loaded" );
+
 }
 
 function onUnload(shutdown) {
 	// console.debug('PT unloading');
 	// Services.console.logStringMessage("onUnload messenger");
+	Services.obs.removeObserver(window.printingtoolsng.printObserver, "subdialog-loaded" );
 
 }
