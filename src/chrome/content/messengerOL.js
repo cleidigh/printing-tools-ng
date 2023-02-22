a1b101d764ed8685fd974f3912e85dd6f5738c1c
Christopher Leidigh <cleidigh@Gmail.com>
2023-02-22 18:43:32 -0500

    Add "Help"

--- src/chrome/content/messengerOL.js
+++ src/chrome/content/messengerOL.js
@@ -76,7 +76,7 @@ function onLoad() {
 		  <menuitem id="ptng-button-printpreview" accesskey="&contextPrintPreview.accesskey;" label="&printPreview.label;" oncommand="printingtools.cmd_printng({printSilent: false}); event.stopPropagation();"  />
 		  <menuseparator />
 		  <menuitem id="ptng-button-options" accesskey="o" label="&ptngOptions.label;" oncommand="openPTdialog(false); event.stopPropagation();"/>
-		  <menuitem id="ptng-button-help" accesskey="h" label="Help" oncommand="utils.loadHelp(); event.stopPropagation();"/>
+		  <menuitem id="ptng-button-help" accesskey="h" label="&Help;" oncommand="utils.loadHelp(); event.stopPropagation();"/>
 		  </menupopup>
 	  </toolbarbutton>
 </toolbarpalette>
