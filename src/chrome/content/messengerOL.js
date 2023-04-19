42a1936982627bf6fe9c39a8d4bb82e207414eb8
Christopher Leidigh <cleidigh@Gmail.com>
2023-04-19 16:39:23 -0400

    comments and testing

--- src/chrome/content/messengerOL.js
+++ src/chrome/content/messengerOL.js
@@ -138,9 +138,13 @@ async function onLoad() {
 
 	let dtdFiles = ["chrome://printingtoolsng/locale/printingtoolsng.dtd", "chrome://messenger/locale/messenger.dtd"];
 
-	
-	addTBbuttonMainFuncOrCtxMenu(ADDON_ID, "unified-toolbar-button", window.printingtools.cmd_printng, ctxMenu, dtdFiles)
+	addTBbuttonMainFuncOrCtxMenu(ADDON_ID, "unified-toolbar-button", window.printingtools.cmd_printng, ctxMenu, dtdFiles);
+
+	// These are the signatures for button function only or context menu only:
+	// addTBbuttonMainFuncOrCtxMenu(ADDON_ID, "unified-toolbar-button", window.printingtools.cmd_printng, null, null);
+	// addTBbuttonMainFuncOrCtxMenu(ADDON_ID, "unified-toolbar-button", null, ctxMenu, dtdFiles);
 }
+
 
 function addTBbuttonMainFuncOrCtxMenu(addOnId, toolbarClass, mainButtFunc, buttCtxMenu, ctxMenuDTDs) {
 	// width of ucarret dropdown area in px
@@ -153,6 +157,12 @@ function addTBbuttonMainFuncOrCtxMenu(addOnId, toolbarClass, mainButtFunc, buttC
 		return false;
 	}
 
+	// The toolbar buttons are added in a lazy fashion. They do not get
+	// placed in the toolbar DOM at install or startup, instead they 
+	// get added the fist time either the 3Pane or a messageDisplay tab
+	// is focused. We therefore use a tabmonitor to listen when we have
+	// our button we can add the listener. We then remove the tabmonitor.
+
 	var tabMonitor = {
 		monitorName: "tbButtonListenerMonitor",
 
@@ -163,7 +173,7 @@ function addTBbuttonMainFuncOrCtxMenu(addOnId, toolbarClass, mainButtFunc, buttC
 		onTabClosing() { },
 
 		async onTabSwitched(newTabInfo, oldTabInfo) {
-			console.log(newTabInfo.mode?.name)
+			// console.log(newTabInfo.mode?.name)
 			if (newTabInfo.mode?.name == "mail3PaneTab" || newTabInfo.mode?.name == "mailMessageTab") {
 				await setup();
 			}
@@ -174,10 +184,19 @@ function addTBbuttonMainFuncOrCtxMenu(addOnId, toolbarClass, mainButtFunc, buttC
 	tabmail.registerTabMonitor(tabMonitor);
 	return true;
 
+	// Setup parent div listener according to parameters.
+	// Wait until button is installed to add listener.
+
 	async function setup() {
-		await new Promise(resolve => window.setTimeout(resolve, 10));
-		let tbExtButton = document.querySelector(`button.${toolbarClass}[extension="${addOnId}"]`);
-		console.log(tbExtButton)
+		var tbExtButton;
+		for (var index = 0; index < 100; index++) {
+			tbExtButton = document.querySelector(`button.${toolbarClass}[extension="${addOnId}"]`);
+			if (tbExtButton) {
+				break;
+			}
+			await new Promise(resolve => window.setTimeout(resolve, 1));
+		}
+
 		if (!tbExtButton) {
 			console("Exception: Extension button not found on toolbar")
 			return;
@@ -206,11 +225,13 @@ function addTBbuttonMainFuncOrCtxMenu(addOnId, toolbarClass, mainButtFunc, buttC
 		tabmail.unregisterTabMonitor(tabMonitor);
 	}
 
+	// Listener function called when on mail type tabs, however we need
+	// to poll for button. Not ideal, but given it's not instant no
+	// beter way. Setup according to call params. Kill unnecessary tabmonitor.
 
 	function listenerFunc(e) {
 		e.stopImmediatePropagation();
 		e.stopPropagation();
-		console.log(e)
 
 		if (e.target.nodeName == "menuitem") {
 			return;
@@ -225,7 +246,7 @@ function addTBbuttonMainFuncOrCtxMenu(addOnId, toolbarClass, mainButtFunc, buttC
 		// get click location and determine if in dropdown window if split button
 		let targetDivBRect = tbExtButton.getBoundingClientRect();
 		let inTargetWindow = e.clientX > (targetDivBRect.x + targetDivBRect.width - dropdownTargetWidth);
-		// setup for context menu
+		// open context menu if configure
 		if ((buttCtxMenu && !mainButtFunc) || (buttCtxMenu && inTargetWindow)) {
 			tbExtButton.nextElementSibling.openPopup(tbExtButton, "after_start", 0, 0, false, false);
 		} else {
@@ -233,6 +254,8 @@ function addTBbuttonMainFuncOrCtxMenu(addOnId, toolbarClass, mainButtFunc, buttC
 		}
 	};
 }
+
+
 
 
 // -- Define listeners for messages from the background script.
