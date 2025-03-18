dcbd3149c4abf6558251ee3a1a4e2015af607e01
Christopher Leidigh <cleidigh@Gmail.com>
2025-03-18 17:47:31 -0400

    fix mail10 import

--- src/chrome/content/printingtoolsng-pengine.js
+++ src/chrome/content/printingtoolsng-pengine.js
@@ -37,7 +37,9 @@ var ietngExtension = ExtensionParent.GlobalManager.getExtension(
 	"ImportExportToolsNG@cleidigh.kokkini.net"
 );
 
-var { MailE10SUtils } = ChromeUtils.importESModule("resource:///modules/MailE10SUtils.sys.mjs");
+var { MailE10SUtils } = Ptng_ESM
+	? ChromeUtils.importESModule("resource:///modules/MailE10SUtils.sys.mjs")
+	: ChromeUtils.import("resource:///modules/MailE10SUtils.js");
 
 var { strftime } = ChromeUtils.importESModule("chrome://printingtoolsng/content/strftime.mjs");
 
