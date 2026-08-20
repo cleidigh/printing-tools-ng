0816ccc5c962be5d218cf9865c426881664e2986
Christopher Leidigh <cleidigh@Gmail.com>
2026-08-20 15:22:07 -0400

    use chrome URL for NotifyTools

--- src/chrome/content/messengerOL.js
+++ src/chrome/content/messengerOL.js
@@ -19,7 +19,7 @@ let extension2 = ExtensionParent.GlobalManager.getExtension(ADDON_ID);
 
 // Load notifyTools into a custom namespace, to prevent clashes with other add-ons.
 window.ptngAddon = {};
-Services.scriptloader.loadSubScript(extension2.rootURI.resolve("chrome/content/notifyTools.js"), window.ptngAddon, "UTF-8");
+Services.scriptloader.loadSubScript("chrome://printingtoolsng/content/notifyTools.js", window.ptngAddon, "UTF-8");
 
 
 async function onLoad() {
