566a3f71f9a520e0d563703fce1c9bf960b6a1c3
Christopher Leidigh <cleidigh@Gmail.com>
2026-08-22 18:47:18 -0400

    debug

--- src/chrome/content/ptng-options.js
+++ src/chrome/content/ptng-options.js
@@ -49,7 +49,10 @@ function setComplexPref(pref, value) {
 async function initPMDpanel() {
 
 	// cleidigh
-	//console.debug('initialize panel');
+	console.debug('initialize panel');
+	console.debug('strftime tests:');
+	let testDate = new Date();
+	let dateformat
 
 	var win = Cc["@mozilla.org/appshell/window-mediator;1"]
 		.getService(Ci.nsIWindowMediator)
